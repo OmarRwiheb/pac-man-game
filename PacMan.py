ae709a3a8264d3a3b1ca96706652893b687ba792
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2021-05-16 21:26:49 +0200

    Add files via upload

--- PacMan.py
+++ PacMan.py
@@ -1,5 +1,5 @@
 import pygame
-from Modules.level import Level
+from Modules.level import Level, Point
 from Modules.mobs import Player
 
 #initializes some standard pygame classes
@@ -26,7 +26,7 @@ screen = pygame.display.set_mode(screen_size)
 running = True
 while running:
 
-#iterate through all events generated from pygame
+    #iterate through all events generated from pygame
     for event in pygame.event.get():
         #quits when user presses the X
         if event.type == pygame.QUIT:
@@ -51,11 +51,73 @@ while running:
 
         elif pressed[pygame.K_RIGHT]:
             player.move("r", level)
+
     #fills the screen with black to to prepare for the next frame
     screen.fill((0, 0, 0))
     
     #iterates through the list of sprites, drawing each image inside its rectangle
     for i in game_group:
         screen.blit(i.image, i.rect)
+
+    points = []
+
+    for i in range(0, 26):
+
+        #Row 1
+        if i not in [0, 12, 13]:
+            points.append(Point(21 + i*16, 20))
+
+        #Row 2
+        points.append(Point(21 + i*16, 85))
+
+        #Row 3
+        if i not in [6, 7, 12, 13, 18, 19]:
+            points.append(Point(21 + i*16, 133))
+
+        #Row 4
+        if i not in [12, 13]:
+            points.append(Point(21 + i*16, 325))
+
+        #Row 5
+        if i not in [3, 4, 21, 22]:
+            points.append(Point(21 + i*16, 374))
+
+        #Row 6
+        if i not in [6, 7, 12, 13, 18, 19]:
+            points.append(Point(21 + i*16, 421))
+
+        #Row 7
+        points.append(Point(21 + i*16, 470))
+
+    for i in range(0, 28):
+
+        #Column 1 and 10
+        if i not in [0, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 22, 23, 24, 25]:
+            points.append(Point(21, 21 + i*16))
+            points.append(Point(421, 21 + i*16))
+
+        #Column 2 and 9
+        if i in [23, 24]:
+            points.append(Point(53, 21 + i*16))
+            points.append(Point(389, 21 + i*16))
+
+        #Column 3 and 8
+        if i not in [0, 4, 7, 19, 22, 25, 26, 27]:
+            points.append(Point(101, 21 + i*16))
+            points.append(Point(341, 21 + i*16))
+
+        #Column 4 and 7
+        if i in [5, 6, 23, 24]:
+            points.append(Point(149, 21 + i*16))
+            points.append(Point(293, 21 + i*16))
+
+        #Column 5 and 6
+        if i in [1, 2, 3, 20, 21, 26, 27]:
+            points.append(Point(197, 21 + i*16))
+            points.append(Point(245, 21 + i*16))
+
+    for p in points:
+        screen.blit(p.image, p.rect)
+
     #updates the frame
     pygame.display.flip()
