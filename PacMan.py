97dc0b173172a0502f07f36e4c979525aa07e368
OmarRwiheb <59856062+OmarRwiheb@users.noreply.github.com>
2021-05-18 04:16:47 +0200

    adding_points_unsolvedBugs

--- PacMan.py
+++ PacMan.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 from Modules.level import Level, Point
 from Modules.mobs import Player
 
@@ -21,6 +22,86 @@ game_group.add(level, player)
 #sets the window size to the size specified in the level object(dimensions of the map image)
 screen_size = level.size
 screen = pygame.display.set_mode(screen_size)
+
+points = []
+
+# boolean 2d array to store points location
+# so any location on the map that contain a point we will make it = True in the 2d array
+points_location = np.zeros((level.size), dtype=bool)
+
+# store the points location in points list and make it true in the 2d list
+for i in range(0, 26):
+
+    # Row 1
+    if i not in [0, 12, 13]:
+        points.append(Point(21 + i * 16, 23))
+        points_location[21 + i * 16, 23] = True
+    
+    #Row 2
+    points.append(Point(21 + i*16, 85))
+    points_location[21 + i*16, 85] = True
+    
+    #Row 3
+    if i not in [6, 7, 12, 13, 18, 19]:
+        points.append(Point(21 + i*16, 133))
+        points_location[21 + i*16, 133] = True
+    
+    #Row 4
+    if i not in [12, 13]:
+        points.append(Point(21 + i*16, 325))
+        points_location[21 + i*16, 325] = True
+    
+    #Row 5
+    if i not in [3, 4, 21, 22]:
+        points.append(Point(21 + i*16, 374))
+        points_location[21 + i*16, 374] = True
+
+    #Row 6
+    if i not in [6, 7, 12, 13, 18, 19]:
+        points.append(Point(21 + i*16, 421))
+        points_location[21 + i*16, 421] = True
+
+    #Row 7
+    points.append(Point(21 + i*16, 470))
+    points_location[21 + i*16, 470] = True
+
+for i in range(0, 28):
+
+    #Column 1 and 10
+    if i not in [0, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 22, 23, 24, 25]:
+        points.append(Point(21, 21 + i*16))
+        points.append(Point(421, 21 + i*16))
+        points_location[21, 21 + i*16] = True
+        points_location[421, 21 + i*16] = True
+    
+    #Column 2 and 9
+    if i in [23, 24]:
+        points.append(Point(53, 21 + i*16))
+        points.append(Point(389, 21 + i*16))
+        points_location[53, 21 + i*16] = True
+        points_location[389, 21 + i*16] = True
+
+    #Column 3 and 8
+    if i not in [0, 4, 7, 19, 22, 25, 26, 27]:
+        points.append(Point(101, 21 + i*16))
+        points.append(Point(341, 21 + i*16))
+        points_location[101, 21 + i*16] = True
+        points_location[341, 21 + i*16] = True
+
+    #Column 4 and 7
+    if i in [5, 6, 23, 24]:
+        points.append(Point(149, 21 + i*16))
+        points.append(Point(293, 21 + i*16))
+        points_location[149, 21 + i*16] = True
+        points_location[293, 21 + i*16] = True
+
+    #Column 5 and 6
+    if i in [1, 2, 3, 20, 21, 26, 27]:
+        points.append(Point(197, 21 + i*16))
+        points.append(Point(245, 21 + i*16))
+        points_location[197, 21 + i*16] = True
+        points_location[245, 21 + i*16] = True
+
 
 #main loop in which the game runs
 running = True
@@ -54,70 +135,26 @@ while running:
 
     #fills the screen with black to to prepare for the next frame
     screen.fill((0, 0, 0))
-    
-    #iterates through the list of sprites, drawing each image inside its rectangle
-    for i in game_group:
-        screen.blit(i.image, i.rect)
 
-    points = []
+    # search if there is any point near to pacman, and if one is found make it false in the 2d list and remove it from points list
+    for i in range(-10, 10):
+        for j in range(-10, 10):
+            if points_location[player.rect.centerx + i, player.rect.centery + j] == True:
+                points_location[player.rect.centerx + i, player.rect.centery + j] = False
+                # search in the whole poins list for this point
+                for k, o in enumerate(points):
+                    if o.rect.x == player.rect.centerx + i:
+                        del points[k]
+                        break
+                break
 
-    for i in range(0, 26):
-
-        #Row 1
-        if i not in [0, 12, 13]:
-            points.append(Point(21 + i*16, 20))
-
-        #Row 2
-        points.append(Point(21 + i*16, 85))
-
-        #Row 3
-        if i not in [6, 7, 12, 13, 18, 19]:
-            points.append(Point(21 + i*16, 133))
-
-        #Row 4
-        if i not in [12, 13]:
-            points.append(Point(21 + i*16, 325))
-
-        #Row 5
-        if i not in [3, 4, 21, 22]:
-            points.append(Point(21 + i*16, 374))
-
-        #Row 6
-        if i not in [6, 7, 12, 13, 18, 19]:
-            points.append(Point(21 + i*16, 421))
-
-        #Row 7
-        points.append(Point(21 + i*16, 470))
-
-    for i in range(0, 28):
-
-        #Column 1 and 10
-        if i not in [0, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 22, 23, 24, 25]:
-            points.append(Point(21, 21 + i*16))
-            points.append(Point(421, 21 + i*16))
-
-        #Column 2 and 9
-        if i in [23, 24]:
-            points.append(Point(53, 21 + i*16))
-            points.append(Point(389, 21 + i*16))
-
-        #Column 3 and 8
-        if i not in [0, 4, 7, 19, 22, 25, 26, 27]:
-            points.append(Point(101, 21 + i*16))
-            points.append(Point(341, 21 + i*16))
-
-        #Column 4 and 7
-        if i in [5, 6, 23, 24]:
-            points.append(Point(149, 21 + i*16))
-            points.append(Point(293, 21 + i*16))
-
-        #Column 5 and 6
-        if i in [1, 2, 3, 20, 21, 26, 27]:
-            points.append(Point(197, 21 + i*16))
-            points.append(Point(245, 21 + i*16))
 
     for p in points:
         screen.blit(p.image, p.rect)
+
+    #iterates through the list of sprites, drawing each image inside its rectangle
+    for i in game_group:
+        screen.blit(i.image, i.rect)
 
     #updates the frame
     pygame.display.flip()
